65493e6d9a35b046cf3a2e6c7dc38d512d81676d
anmolbhalla <anmol9599@live.com>
2018-03-23 22:43:49 +0530

    Separate file to update db

--- Crypto_Tracker/cryptodisplay/views.py
+++ Crypto_Tracker/cryptodisplay/views.py
@@ -12,14 +12,7 @@ def index (request):
     context = {
         'cryptos_detail': cryptos_detail,
     }
-    with open('/home/anmol/Documents/Github/CryptoTracker/Crawler/crypto_data.txt') as openfileobject:
-        for line in openfileobject:
-            line_split = line.split('***')
-            add=Cryptos(crypto_logo = line_split[0], crypto_graph = line_split[1], crypto_number = line_split[2], crypto_name = line_split[3], crypto_price = line_split[4],
-                        crypto_change = line_split[5], crypto_market_cap = line_split[6], crypto_supply = line_split[7], crypto_volume = line_split[8]
 
-                        )
-            add.save()
 
     return HttpResponse(template.render(context, request))
 
