3bf3d4e297026928b2d74cc3d7a656406408a646
aryansharma23124 <aryansharma23124@gmail.com>
2018-03-23 11:34:10 +0530

    added testing template

--- Crypto_Tracker/cryptodisplay/views.py
+++ Crypto_Tracker/cryptodisplay/views.py
@@ -3,7 +3,15 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from .models import Cryptos
 
 def index (request):
-    return HttpResponse("Welcome to crypto display")
+    cryptos_detail = Cryptos.objects.order_by('id')
+    template = loader.get_template('cryptodisplay/home.html')
+    context = {
+        'cryptos_detail': cryptos_detail,
+    }
+    return HttpResponse(template.render(context, request))
+
+
 # Create your views here.
