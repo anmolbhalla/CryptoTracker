258928c94882e7a32ec75d7a8a01ae749327decd
Arsalan099 <khan038299@gmail.com>
2018-03-23 20:57:41 +0530

    Added coin market header and details

--- Crawler/market_crawler.py
+++ Crawler/market_crawler.py
@@ -6,43 +6,61 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 
-driver=webdriver.Chrome()
-url = 'https://coinmarketcap.com/currencies/ethereum/#markets'
-driver.get(url)
-timeout=200
+def Coin_Market_Details():
+    data_file=open('crypto_market_data.txt','w')
 
-try:
-    element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[1]/div[5]'))
-    WebDriverWait(driver, timeout).until(element_present)
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[1]/div[5]'))
+        WebDriverWait(driver, timeout).until(element_present)
 
-except TimeoutException:
-    print("May be you are not connected to internet")
+    except TimeoutException:
+        print("May be you are not connected to internet")
 
 
-coin_details_table = driver.find_elements_by_xpath('//*[@id="markets-table"]')
-coin_name = driver.find_element_by_class_name('text-large').text
-coin_price = driver.find_elements_by_class_name('text-large2')
-coin_currency = driver.find_element_by_class_name('details-text-medium').text
-coin_marketcap = driver.find_elements_by_class_name("coin-summary-item-detail")
+    coin_details_table = driver.find_elements_by_xpath('//*[@id="markets-table"]')
+    coin_name = driver.find_element_by_class_name('text-large').text
 
-print(coin_name)
-print(coin_price[0].text+coin_currency)
-print(coin_price[1].text)
+    for i in range(0,len(coin_details_table)):
 
-for i in coin_marketcap:
+         tag = coin_details_table[i].find_elements_by_tag_name('tr')
 
-    print(i.text)
+         for j in range(0,len(tag)):
 
-for i in range(0,len(coin_details_table)):
+             table_data=tag[j].find_elements_by_tag_name('td')
+             if(j!=0):data_file.writelines(str(coin_name) + '-')
 
-     tag = coin_details_table[i].find_elements_by_tag_name('tr')
+             for k in range(0,len(table_data)-1):
 
-     for i in range(0,len(tag)):
+                 data_file.writelines(str(table_data[k].text) + '-')
+    data_file.close()
 
-         table_data=tag[i].find_elements_by_tag_name('td')
+def Coin_Market_Headers():
+    data_file = open('crypto_market_data_headers.txt', 'w')
 
-         for i in range(0,len(table_data)-1):
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[1]/div[5]'))
+        WebDriverWait(driver, timeout).until(element_present)
 
-             print(table_data[i].text)
+    except TimeoutException:
+        print("May be you are not connected to internet")
 
-         print('\n')
+    coin_name = driver.find_element_by_class_name('text-large').text
+    coin_price = driver.find_elements_by_class_name('text-large2')
+    coin_currency = driver.find_element_by_class_name('details-text-medium').text
+    coin_marketcap = driver.find_elements_by_class_name("coin-summary-item-detail")
+    coin_market_header = driver.find_elements_by_class_name("coin-summary-item-header")
+
+    data_file.writelines(str(coin_name) + '-')
+    data_file.writelines(str(coin_price[0].text) + coin_currency + '-')
+    data_file.writelines(str(coin_price[1].text + '\n'))
+
+    for i in range(0,len(coin_marketcap)):
+        data_file.writelines(str(coin_market_header[i].text) + '-' + str(coin_marketcap[i].text + '\n'))
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    url = 'https://coinmarketcap.com/currencies/iconomi/#markets'
+    driver.get(url)
+    timeout = 200
+    Coin_Market_Details()
+    Coin_Market_Headers()
