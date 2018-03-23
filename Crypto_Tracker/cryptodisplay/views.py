a56bfa7901199eb151db6631bcf67706341eda59
aryansharma23124 <aryansharma23124@gmail.com>
2018-03-23 21:05:24 +0530

    reading data from file

--- Crypto_Tracker/cryptodisplay/views.py
+++ Crypto_Tracker/cryptodisplay/views.py
@@ -11,6 +11,16 @@ def index (request):
     context = {
         'cryptos_detail': cryptos_detail,
     }
+    with open('/home/aryan/Documents/Github/CryptoTracker/Crawler/crypto_data.txt') as openfileobject:
+        for line in openfileobject:
+            line_split = line.split('-')
+           # print(line_split)
+            add=Cryptos(crypto_logo=line_split[0],crypto_graph=line_split[1],crypto_name=line_split[3],crypto_market_cap=line_split[3],
+                        crypto_price=line_split[4],crypto_volume=line_split[5],crypto_supply=line_split[6]
+
+                        )
+            add.save()
+
     return HttpResponse(template.render(context, request))
 
 
