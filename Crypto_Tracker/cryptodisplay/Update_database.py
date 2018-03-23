6d43058d20c69daa64d275f5ef2aa06aec76c369
aryansharma23124 <aryansharma23124@gmail.com>
2018-03-23 23:10:06 +0530

    Displaying details on the home page from the Crypto Model

--- Crypto_Tracker/cryptodisplay/Update_database.py
+++ Crypto_Tracker/cryptodisplay/Update_database.py
@@ -9,7 +9,7 @@ def Update_Database () :
 
     delete = Cryptos.objects.all().delete()
 
-    with open('/home/anmol/Documents/Github/CryptoTracker/Crawler/crypto_data.txt') as openfileobject:
+    with open('/home/aryan/Documents/Github/CryptoTracker/Crawler/crypto_data.txt') as openfileobject:
         count = 1
 
         for line in openfileobject:
--- Crypto_Tracker/cryptodisplay/models.py
+++ Crypto_Tracker/cryptodisplay/models.py
@@ -2,18 +2,6 @@ from django.db import models
 
 
 class Cryptos(models.Model):
-<<<<<<< HEAD
-    crypto_id=models.CharField(max_length=200)
-    crypto_logo=models.CharField(max_length=1000)
-    crypto_name=models.CharField(max_length=500)
-    crypto_market_cap=models.CharField(max_length=500)
-    crypto_price=models.CharField(max_length=500)
-    crypto_volume=models.CharField(max_length=500)
-    crypto_supply=models.CharField(max_length=500)
-    crypto_change=models.CharField(max_length=500)
-    crypto_graph=models.CharField(max_length=1000)
-=======
-
     crypto_number = models.CharField(max_length=10, null=True)
     crypto_logo = models.CharField(max_length=1000)
     crypto_name = models.CharField(max_length=500)
@@ -23,7 +11,6 @@ class Cryptos(models.Model):
     crypto_supply = models.CharField(max_length=500)
     crypto_change = models.CharField(max_length=500)
     crypto_graph = models.CharField(max_length=1000)
->>>>>>> 65493e6d9a35b046cf3a2e6c7dc38d512d81676d
 
 
 
