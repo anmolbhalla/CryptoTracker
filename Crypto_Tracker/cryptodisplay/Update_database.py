fec8a238701fba70db336c1cfd6e5dfe79ec7a97
aryansharma23124 <aryansharma23124@gmail.com>
2018-03-26 23:09:00 +0530

    Updated Logic for Model Update

--- Crypto_Tracker/cryptodisplay/Update_database.py
+++ Crypto_Tracker/cryptodisplay/Update_database.py
@@ -8,24 +8,16 @@ from cryptodisplay.models import Cryptos
 
 def Update_Database () :
 
-    delete = Cryptos.objects.all().delete()
+    #delete = Cryptos.objects.all().delete()
 
-    with open('/home/anmol/Documents/Github/CryptoTracker/Crawler/crypto_data.txt') as openfileobject:
-        count = 1
+    with open('/home/aryan/Documents/Github/CryptoTracker/Crawler/crypto_data.txt') as openfileobject:
+        #count = 1
 
         for line in openfileobject:
             line_split = line.split('***')
+            #print(line_split[2])
 
-            add = Cryptos(id=count, crypto_logo=line_split[0] , crypto_graph=line_split[1], crypto_number=line_split[2],
-                          crypto_name=line_split[3], crypto_price=line_split[4],
-                          crypto_change=line_split[5], crypto_market_cap=line_split[6], crypto_supply=line_split[7],
-                          crypto_volume=line_split[8])
 
-            add.save()
 
-            count += 1
 
-while (True) :
-
-    Update_Database()
-    time.sleep(120)
+Update_Database()
--- Crypto_Tracker/cryptodisplay/models.py
+++ Crypto_Tracker/cryptodisplay/models.py
@@ -11,9 +11,16 @@ class Cryptos(models.Model):
     crypto_supply = models.CharField(max_length=500)
     crypto_change = models.CharField(max_length=500)
     crypto_graph = models.CharField(max_length=1000)
+    value_change=models.CharField(max_length=100)
 
 
 
 
 
 # Create your models here.
+
+# name_crypto_id = Cryptos.objects.get(crypto_number=line_split[2])
+#
+# if (name_crypto_id.crypto_name != line_split[3] or name_crypto_id.crypto_price != line_split[
+#     4] or name_crypto_id.crypto_change != line_split[5] or name_crypto_id.crypto_market_cap != line_split[
+#     6] or name_crypto_id.crypto_supply != line_split[7])
